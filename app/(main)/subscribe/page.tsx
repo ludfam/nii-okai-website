6059f2c469cbe91504d9aab1a2e8d8cc1252482b
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-02-06 10:09:18 +0000

    subscribe page

--- app/(main)/subscribe/page.tsx
+++ app/(main)/subscribe/page.tsx
@@ -1,9 +1,20 @@
 import UnderDevelopment from "@/components/core/under-development";
+import NewsletterForm from "@/components/forms/newsletter-form";
+import Heading from "@/components/hierarchy/heading";
 
 export default function SubscribePage() {
     return (
-        <main className="min-h-screen ">
-            <UnderDevelopment />
+        <main className=" ">
+            <section className="min-h-[90vh] px-8 py-8 flex flex-col gap-8 items-center bg-black text-white justify-center">
+                <Heading title="Subscribe" opacity={30} color="white" />
+                <p className="mt-2 md:text-lg text-center">
+                    Sign up to receive the latest news from Nii Okai
+                </p>
+
+                <NewsletterForm />
+
+            </section>
+            {/* <UnderDevelopment /> */}
         </main>
     )
 }
