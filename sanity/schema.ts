c22575f186b63d5c1f5db39581202bed6150a06e
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-03-29 09:13:50 +0000

    schema

---
+++ sanity/schema.ts
@@ -0,0 +1,17 @@
+import { type SchemaTypeDefinition } from 'sanity';
+
+import blockContent from './schemaTypes/blockContent';
+import category from './schemaTypes/category';
+import post from './schemaTypes/post';
+import author from './schemaTypes/author';
+import homePage from './schemaTypes/home-page';
+
+export const schema: { types: SchemaTypeDefinition[]; } = {
+  types: [
+    post, 
+    author, 
+    category, 
+    blockContent, 
+    homePage
+  ],
+};
