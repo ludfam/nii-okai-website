e7d53d7c5bf774fae704129fd45eafcd6db18226
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-04-19 20:46:58 +0000

    export library for schema types

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+export { default as CATEGORY } from "./category";
+export { default as NEWS } from "./news";
+
+export { default as HOMEPAGE } from "./home-page";
+export { default as ABOUTPAGE } from "./about-page";
