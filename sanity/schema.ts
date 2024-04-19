2564b7b1fd97e8abd39204e41dcae76b5d514e88
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-04-19 20:52:56 +0000

    update schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,18 +1,21 @@
 import { type SchemaTypeDefinition } from 'sanity';
-
+import { HOMEPAGE, ABOUTPAGE } from './schemaTypes';
 import blockContent from './schemaTypes/blockContent';
 import category from './schemaTypes/category';
 import news from './schemaTypes/news';
-import author from './schemaTypes/author';
-import homePage from './schemaTypes/home-page';
+import longContent from './schemaTypes/longContent';
 
 export const schema: { types: SchemaTypeDefinition[]; } = {
   types: [
     blockContent, 
+    longContent,
+    
+    HOMEPAGE,
+    ABOUTPAGE,
+
     category, 
     news,
 
-    homePage,
     
   ],
 };
