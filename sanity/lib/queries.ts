14dabd28223cb929fad587f0ba9f8ffae904cbd2
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-04-19 20:52:35 +0000

    sort queries by order

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -6,4 +6,6 @@ export const POSTS_QUERY = groq`*[_type == "post" && defined(slug)]`;
 
 export const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
 
-export const HOMEPAGE_QUERY = groq`*[_type == "homePage"][0]`
+export const HOMEPAGE_QUERY = groq`*[_type == "homePage"] | order(_createdAt asc)[0]`;
+
+export const ABOUTPAGE_QUERY = groq`*[_type == "aboutPage"] | order(_createdAt asc)[0]`;
