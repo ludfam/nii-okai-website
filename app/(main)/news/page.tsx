291cf1faf9f9694f775831745a77df270c6a1c3a
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-05-07 23:24:18 +0000

    news page structure

--- app/(main)/news/page.tsx
+++ app/(main)/news/page.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { SanityDocument } from "next-sanity";
+import { client } from "@/sanity/lib/client";
+import { generatePageQuery } from "@/sanity/lib/queries";
+import SanityImage from "@/components/core/sanity-image";
+import { News } from "@/types";
+import NewsCard from "@/components/cards/news-card";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+    const data = await client.fetch<SanityDocument<News>[]>(generatePageQuery("news", "_createdAt", "asc", "none"));
     return (
-        <main className="min-h-screen">
+        <main className="min-h-screen flex flex-col gap-8">
+            <section className="w-full min-h-[40vh] border-b flex flex-col gap-4 items-center justify-center h-full ">
+                <p className="text-base font-semibold text-o-lightblue uppercase">get information</p>
+                <h1 className="text-5xl font-bold tracking-tight text-gray-900 sm:text-5xl allexis ">From The Blog </h1>
+            </section>
+            <section className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {
+                    (data || [])?.map((news) => (
+                        <NewsCard key={news._id} data={news} />
+                    ))
+                }
+            </section>
 
         </main>
     )
